1e021afca27597573f6888df318f90f1451c1632
saranghe41 <saranghe-0-@hanmail.net>
2021-11-18 15:18:59 +0900

    api를 이용한 사진, 사용자 검색 프로젝트 - ui 작업

--- UIKit_Fundamental/ViewController.swift
+++ UIKit_Fundamental/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var segmentBtn: UISegmentedControl!
+    @IBOutlet weak var myTextField: UITextField!
+    
+    @objc fileprivate func onSegmentBtnClicked() {
+        print("ViewController - onSegmentBtnClicked() called / segmentIndex: \(segmentBtn.selectedSegmentIndex)")
+        
+        if (self.segmentBtn.selectedSegmentIndex == 0) {
+            self.myTextField.placeholder = "사진 키워드 입력"
+        }
+        else if (self.segmentBtn.selectedSegmentIndex == 1) {
+            self.myTextField.placeholder = "사용자 키워드 입력"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        print("ViewController - viewDidLoad() called")
+        
+        segmentBtn.addTarget(self, action: #selector(onSegmentBtnClicked), for: .touchUpInside)
     }
-
-
 }
 
